d3550c44bbb919b2a3762710fcb2e0d0e9b062fc
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-02-01 13:17:02 +0900

    하이라이트 동시 접속 구현

--- utils/user.js
+++ utils/user.js
@@ -1,11 +1,19 @@
-const users = [];
+let users = [];
 
 // Join user to chat
-const userJoin = (id, roomId, bookId, userId, userName, host, presenter) => {
-	const user = { id, roomId, bookId, userId, userName, host, presenter };
-
+const userJoin = (id, roomId, bookId, memberId, userId, userName, host, presenter) => {
+	const user = { id, roomId, bookId, memberId, userId, userName, host, presenter };
 	users.push(user);
 	return user;
+};
+
+const userChange = (newUser) => {
+	users = users.map((user) => {
+		if (user.userId === newUser.userId) {
+			return newUser; // userId가 일치하면 newUser로 변경
+		}
+		return user; // 그렇지 않으면 기존 사용자를 반환
+	});
 };
 // User leaves chat
 const userLeave = (id) => {
@@ -31,6 +39,7 @@ const getUsers = (roomId) => {
 
 module.exports = {
 	userJoin,
+	userChange,
 	userLeave,
 	getUsers,
 };
