4ef570211259fc3f7ea4e6ab6f7d674715810860
Youjin Jo <youjinjoy@gmail.com>
2024-02-07 13:55:56 +0900

    캔버스 합치기, 차트 고치기

--- socket.js
+++ socket.js
@@ -51,21 +51,25 @@ module.exports = (server) => {
 		});
 
 		//pointer
-		socket.on("movepointer", (data) => {
+		socket.on("move-pointer", (data) => {
+			console.log("data", data);
 			// 커서 위치와 클라이언트 ID 매핑
 			const pointerData = {
-				id: user.id,
-				color: colors[user.id % colors.length], // 색상 추가
+				id: data.userId,
+				color: colors[data.userId % colors.length], // 색상 추가
 				book: data.book,
 				page: data.page,
 				x: data.x,
 				y: data.y,
 			};
-			io.emit("updatepointer", pointerData);
+			io.emit("update-pointer", pointerData);
 		});
 
 		socket.on("drawing", (data) => {
-			socket.broadcast.to(data.location.roomId).emit("canvasImage", data);
+			console.log("roomId", data.location.roomId);
+			if (data) {
+				io.to(data.location.roomId).emit("canvasImage", data);
+			}
 		});
 	});
 	return io;
