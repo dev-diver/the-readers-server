013a3b35df1f1d827800171f5a33b7d5393864e0
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-02-10 19:48:50 +0900

    cursor fix

--- socket.js
+++ socket.js
@@ -61,16 +61,18 @@ module.exports = (server) => {
 
 		//pointer
 		socket.on("move-pointer", (data) => {
-			console.log("data", data);
+			let color = colors[data.user.id % colors.length];
+			// console.log("color", color);
 			// 커서 위치와 클라이언트 ID 매핑
 			const pointerData = {
-				id: data.userId,
-				color: colors[data.userId % colors.length], // 색상 추가
+				user: data.user,
+				color: colors[data.user.id % colors.length], // 색상 추가
 				bookId: data.bookId,
 				pageNum: data.pageNum,
 				x: data.x,
 				y: data.y,
 			};
+			// console.log("data", pointerData);
 			io.emit("update-pointer", pointerData);
 		});
 
